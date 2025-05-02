5513432fac1c88b0146ddc07de077da3095792a7
houssem-troudi-dev <HoussemEddine.TROUDI@esprit.tn>
2025-05-02 23:16:44 +0100

    added charathere 2

---
+++ jumpright/charather3.h
@@ -0,0 +1,119 @@
+#ifndef CHARATHER3_H_INCLUDED
+#define CHARATHER3_H_INCLUDED
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include <SDL/SDL_ttf.h>
+
+#define THROW_ANTICIP_FRAMES   5
+#define THROW_EXEC_FRAMES      3
+#define THROW_RECOV_FRAMES     4
+#define PROJ_FRAMES            3
+#define MAX_HEALTH             3
+#define MAX_DEATH_FRAMES       12
+#define ANIMAL_FRAMES          10  
+typedef struct {
+    SDL_Surface *idleLeft[ANIMAL_FRAMES];
+    SDL_Surface *idleRight[ANIMAL_FRAMES];
+    SDL_Surface *moveLeft[ANIMAL_FRAMES];
+    SDL_Surface *moveRight[ANIMAL_FRAMES];
+    SDL_Rect position;
+    int currentFrame;
+    int animationSpeed;
+    Uint32 lastFrameTime;
+    int isMoving;
+    int direction;
+} Animal;
+typedef struct 
+{   Animal pet;
+    SDL_Rect positionimage;
+    SDL_Surface *idleleftImages[5];
+    SDL_Surface *idlerightImages[5];
+    SDL_Surface *runleftImages[8];
+    SDL_Surface *runrightImages[8];
+    SDL_Surface *hitLeftImages[7];
+    SDL_Surface *hitRightImages[7];
+    SDL_Surface *jumpleftImages[7];
+    SDL_Surface *jumprightImages[7];
+
+  
+    SDL_Surface *healthImages[MAX_HEALTH];
+    SDL_Surface *deathAnimation[MAX_DEATH_FRAMES];
+    int health;
+    int isDead;
+    int deathAnimPlaying;
+    int deathFrame;
+
+    int count0, count1, count2, count3;
+
+    int isJumping;
+    int onGround;
+    int yVelocity;
+    int gravity;
+    int jumpHeight;
+    int frameJump;
+    int jumpAnimCounter;
+    int jumpPhase;
+
+    int frameidleleft, frameidleright, framerunleft, framerunright;
+    int frameHitLeft, frameHitRight;
+    int ismoving;
+    int ismovingL, ismovingR;
+    int isHittingLeft, isHittingRight;
+    int oldwav;
+    Mix_Chunk *hitSound;
+    int compteurhitleft;
+    int compteurhitright;    
+    Mix_Chunk *jumpsound;
+    int jumpSoundPlayed;
+    Mix_Chunk *runsound;
+    int walkSoundPlayedleft;
+    int walkSoundPlayedright;
+    int firsttime;
+    Mix_Chunk *throwSound;
+    int throwSoundPlayed;
+    Mix_Chunk *throwSound1;
+
+    SDL_Surface *throwAnticipLeft[THROW_ANTICIP_FRAMES];
+    SDL_Surface *throwExecLeft[THROW_EXEC_FRAMES];
+    SDL_Surface *throwRecovLeft[THROW_RECOV_FRAMES];
+    SDL_Surface *projFramesLeft[PROJ_FRAMES];
+    SDL_Surface *throwAnticipRight[THROW_ANTICIP_FRAMES];
+    SDL_Surface *throwExecRight[THROW_EXEC_FRAMES];
+    SDL_Surface *throwRecovRight[THROW_RECOV_FRAMES];
+    SDL_Surface *projFramesRight[PROJ_FRAMES];
+
+    int isThrowing;
+    int isThrowingLeft;
+    int isThrowingRight;
+    int throwPhase;      
+    int throwFrame;
+    Uint32 throwTimer;    
+
+    int projActive;
+    int projX, projY;
+    int projDir;       
+    int projFrame;
+    Uint32 projTimer;
+    int canThrow;
+    int isSprinting;
+} charathere1;
+
+void init_charather(charathere1 *c1);
+void affichercharthere1(SDL_Surface *screen, SDL_Surface *background_principale, charathere1 *c1);
+void collisionavecclavier(charathere1 *c1);
+void miseajourcharthere(charathere1 *c1);
+void libererCharathere(charathere1 *c1, SDL_Surface *background);
+
+
+void init_health_system(charathere1 *c1);
+void update_health(charathere1 *c1);
+void render_health(SDL_Surface *screen, charathere1 *c1);
+void render_death_animation(SDL_Surface *screen, charathere1 *c1);
+
+
+void init_animal(Animal *animal, charathere1 *c1);
+void update_animal(Animal *animal, charathere1 *c1);
+void render_animal(SDL_Surface *screen, Animal *animal);
+
+#endif
---
+++ jumpright/main.c
@@ -0,0 +1,86 @@
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <stdio.h>
+#include <SDL/SDL_mixer.h>
+#include "charather3.h"
+
+int main(int argc, char *argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        printf("SDL_image could not initialize! IMG_Error: %s\n", IMG_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+    SDL_Surface *screen = SDL_SetVideoMode(1920, 1080, 32, SDL_SWSURFACE);
+    if (screen == NULL) {
+        printf("Unable to set video mode! SDL_Error: %s\n", SDL_GetError());
+        IMG_Quit();
+        SDL_Quit();
+        return -1;
+    }
+
+    SDL_Surface *background_principale = IMG_Load("background_principale.png");
+    if (background_principale == NULL) {
+        printf("Error loading background image: %s\n", IMG_GetError());
+        IMG_Quit();
+        SDL_Quit();
+        return -1;
+    }
+
+
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) < 0) {
+        printf("Failed to initialize SDL_mixer: %s\n", Mix_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    Mix_Music *music = Mix_LoadMUS("hollowknight.mp3");
+    if (music == NULL) {
+        printf("Failed to load music: %s\n", Mix_GetError());
+        Mix_CloseAudio();
+        SDL_Quit();
+        return 1;
+    }
+
+    Mix_PlayMusic(music, -1);
+    Mix_VolumeMusic(64);
+
+    charathere1 c1;
+    init_charather(&c1);
+    
+    SDL_Event event;
+    int quitter = 0;
+    
+    while (!quitter) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT)
+                quitter = 1;
+            else if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.sym == SDLK_ESCAPE)
+                    quitter = 1;
+                else if (event.key.keysym.sym == SDLK_u && !c1.isDead)
+                    update_health(&c1);
+            }
+        }
+
+        if (!c1.isDead || c1.deathAnimPlaying) {
+            collisionavecclavier(&c1);
+            miseajourcharthere(&c1);
+            affichercharthere1(screen, background_principale, &c1);
+        }
+        
+        SDL_Delay(30);
+    }
+
+    libererCharathere(&c1, background_principale);
+    Mix_FreeMusic(music);
+    Mix_CloseAudio();
+    SDL_Quit();
+    
+    return 0;
+}
